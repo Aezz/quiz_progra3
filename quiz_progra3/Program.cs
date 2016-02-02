8be4fac5ade7876e35e4bebd7a877526c7ba334a
Aezz <adrianzunigaz@hotmail.com>
2016-02-01 19:34:01 -0600

    Ver1.0
    Consola

---
+++ quiz_progra3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz_progra3
+{
+    public class Person
+    {
+        public string name;
+
+        public Person()
+        {
+            name = "unknown";
+        }
+        public Person(string nm)
+        {
+            name = nm;
+        }
+        public void Setname(string newName)
+        {
+            name = newName;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Console.WriteLine(person1.name);
+
+            person1.Setname("Adrian Zúñiga Zamora");
+            Console.WriteLine(person1.name);
+
+            Person person2 = new Person("Alejandra Ramirez");
+            Console.WriteLine(person2.name);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
